155f6e38617d30ac9d180fb61355c0ca644c8812
Elijah Perez <ElijahjacobPerez@gmail.com>
2025-01-15 17:33:44 -0800

    Pushing code done as SSC

--- Robotics/Labs/Lab1_LinesAndSegments/computeDistancePointToPolygon.m
+++ Robotics/Labs/Lab1_LinesAndSegments/computeDistancePointToPolygon.m
@@ -1,4 +1,4 @@
-function D = computeDistancePointToPolygon(P,q)
+function [D,Segment] = computeDistancePointToPolygon(P,q)
 %% Error checking
 if length(P(1,:)) ~= 2
 error ("Dimensions of P must be [x1 y1, x2 y2, ... xn yn]")
@@ -26,14 +26,24 @@ for i = 1:length(P(:,1))
     [dist(i),point(i,:)] = computeDistancePointToSegment(q,p1,p2);
 end
 
+
 %% output
 D = min(dist);
-finder = dist == D;
-point = point(finder,:);
+
+finder  = dist == D;
+point   = point(finder,:);
+
+if length(point(:,1)) == 1
+Segment(1,:) = P(finder,:);
+point2 = find(finder ==1)+1;
+Segment(2,:) = P(point2,:);
+else 
+Segment = point(1,:);
+end
+
 
 
 %% Plotting 
-
 figure 
 X = P(:,1);
 Y = P(:,2);
